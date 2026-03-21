7593993995b68e168784de97eea551c013ef8f1e
f4lga <rinagodulyan@gmail.com>
2026-03-21 23:08:45 +0300

    feat: integrate engine and world, add graceful shutdown

---
+++ internal/network/transport.go
@@ -0,0 +1,9 @@
+package network
+
+import "ssh-arena-app/internal"
+
+// NetworkTransport returns a stub implementation of the NetworkTransport interface.
+func NetworkTransport() internal.NetworkTransport {
+	// TODO: implement
+	return nil
+}
---
+++ internal/ui/renderer.go
@@ -0,0 +1,9 @@
+package ui
+
+import "ssh-arena-app/internal"
+
+// Renderer returns a stub implementation of the Renderer interface.
+func Renderer() internal.Renderer {
+	// TODO: implement
+	return nil
+}
